916ca2d21742d176f90e7da2280294f5d1b82ec2
meri96bockn <meri96bockn@gmail.com>
2022-01-27 07:18:51 +0900

    featuresのリンク名を直した。

--- work/web/index.php
+++ work/web/index.php
@@ -16,7 +16,7 @@ include('../app/_parts/_header.php');
   <h1>Dream<br>Museum</h1>
   <p>夢を展示する博物館</p>
   <div class="join">
-    <a href="join.html">新規登録</a>
+    <a href="join.php">新規登録</a>
   </div>
 </div>
 </div>
@@ -38,7 +38,7 @@ include('../app/_parts/_header.php');
     <i class="bi bi-image-fill"></i>
   </div>
   <div class="top_feature_link">
-    <a href="dreams.html">寄贈された夢を見る</a>
+    <a href="dreams.php">寄贈された夢を見る</a>
   </div>
 </div>
 
@@ -58,7 +58,7 @@ include('../app/_parts/_header.php');
     <i class="bi bi-journal-richtext"></i>
   </div>
   <div class="top_feature_link">
-    <a href="howto.html">夢日記のつけ方を見る</a>
+    <a href="howto.php">夢日記のつけ方を見る</a>
   </div>
 </div>
 </div>
