9c40692d2c6e67c4e20dc8643201d9f78cc7cb7c
meri96bockn <meri96bockn@gmail.com>
2022-02-02 15:09:10 +0900

    再入力用パスワードを追加
    パスワードを2回入力する。
    1回目のパスワードと一致しなければエラーが出るようになっている。
    また、パスワードを間違えた場合は入力内容が空になる。
    一方、パスワード以外を間違えた時は入力内容が保持される。

--- work/public/join.php
+++ work/public/join.php
@@ -7,10 +7,11 @@ $form = [
   'email' => '',
   'password' => ''
 ];
+$re_password = '';
 $error = [];
 
 $form['name'] = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
-if (!preg_match('/\A[a-z\d]{4,100}+\z/i', $form['name'])) {
+if (!preg_match('/\A[a-z\d]{1,100}+\z/i', $form['name'])) {
   $error['name'] = 'alphanumeric';
 }
 
@@ -22,67 +23,87 @@ if ($form['email'] === '') {
 $form['password'] = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
 if (!preg_match('/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,100}+\z/i', $form['password'])) {
   $error['password'] = 'alphanumeric';
-}
+  $form['password'] = '';
+} 
 
+$re_password = filter_input(INPUT_POST, 're_password', FILTER_SANITIZE_STRING);
+if ($form['password'] !== $re_password) {
+  $error['re_password'] = 'miss';
+  $re_password = '';
+}
 
 if (empty($error)) {
   $_SESSION['form'] = $form;
-  header('location: my_page.php');
+  header('location: login.php');
   exit();
 }
 
-
 $title = '新規登録 - ';
 $this_css = 'form';
-include('../app/_parts/_header.php');
+include(__DIR__ . '/../app/_parts/_header.php');
+
+// var_dump($re_password);
+var_dump($form['password']);
+// var_dump($form);
+var_dump($error);
+var_dump($re_password);
 ?>
 
-  <div class="forms">
-    <div class="form_title">
-      <h1>新規登録</h1>
-    </div>
-    <div class="form">
-      <form action="" method="post" enctype="multipart/form-data">
-        <div class="form_item">
-          <label for="name">ユーザーネーム</label>
-          <input type="text" name="name" id="name" maxlength="255" placeholder="（例）yume3" value="<?= h($form['name']); ?>" >
-            <div class="error">
-              <?php if (isset($error['name']) && $error['name'] === 'alphanumeric'):?>
-                <p class="error">* ユーザーネームは半角英数4文字以上で入力してください</p>
-              <?php endif; ?>
-          </div>
-        </div>
-
-
-        <div class="form_item">
-          <label for="email">メールアドレス</label>
-          <input type="email" name="email" id="email" maxlength="255" placeholder="（例）yumemi@gmail.com" value="<?= h($form['email']); ?>">
-          <div class="error">
-            <?php if(isset($error['email']) && $error['email'] === 'blank'): ?>
-            <p class="error">* メールアドレスを入力してください</p>
-            <?php endif; ?>
-            <p class="error">* ご指定のメールアドレスはすでに登録されています</p>
-          </div>
-        </div>
-        <div class="form_item">
-          <label for="password">パスワード</label>
-          <input type="password" name="password" id="password" maxlength="20" placeholder="（例）yume36ko" value="<?= h($form['password']); ?>">
-          <div class="error">
-            <?php if (isset($error['password']) && $error['password'] === 'alphanumeric'):?>
-            <p class="error">* パスワードは半角英数を組み合わせ、8文字以上で入力してください</p>
-            <?php endif; ?>
-          </div>
-        </div>
-        <button>登録</button>
-      </form>
-    </div>
+<div class="forms">
+  <div class="form_title">
+    <h1>新規登録</h1>
   </div>
+  <div class="form">
+    <form action="" method="post" enctype="multipart/form-data">
+      <div class="form_item">
+        <label for="name">ユーザーネーム</label>
+        <input type="text" name="name" id="name" maxlength="255" placeholder="（例）yume3" value="<?= h($form['name']); ?>" >
+          <div class="error">
+            <?php if (isset($error['name']) && $error['name'] === 'alphanumeric'):?>
+              <p class="error">* ユーザーネームは半角英数で入力してください</p>
+            <?php endif; ?>
+        </div>
+      </div>
+      <div class="form_item">
+        <label for="email">メールアドレス</label>
+        <input type="email" name="email" id="email" maxlength="255" placeholder="（例）yumemi@gmail.com" value="<?= h($form['email']); ?>">
+        <div class="error">
+          <?php if(isset($error['email']) && $error['email'] === 'blank'): ?>
+          <p class="error">* メールアドレスを入力してください</p>
+          <?php endif; ?>
+          <p class="error">* ご指定のメールアドレスはすでに登録されています</p>
+        </div>
+      </div>
+      <div class="form_item">
+        <label for="password">パスワード</label>
+        <input type="password" name="password" id="password" placeholder="（例）yume36ko" value="<?= h($form['password']); ?>">
+        <div class="error">
+          <?php if (isset($error['password']) && $error['password'] === 'alphanumeric'): ?>
+          <p class="error">* パスワードは半角英数を組み合わせ、8文字以上で入力してください</p>
+          <?php endif; ?>
+        </div>
+      </div>
+      <div class="form_item">
+        <label for="re_password">パスワード（再入力）</label>
+        <input type="password" name="re_password" id="re_password" placeholder="（例）yume36ko" value="<?= h($re_password);?>"
+        >
+        <div class="error">
+          <?php if (isset($error['re_password']) && $error['re_password'] === 'miss'):?>
+          <p class="error">* パスワードが一致していません</p>
+          <?php endif; ?>
 
-  <?php
+        </div>
+      </div>
+      <button>登録</button>
+    </form>
+  </div>
+</div>
 
-  include('../app/_parts/_footer.php');
+<?php
 
-  ?>
-  <script src="js/main.js"></script>
+include('../app/_parts/_footer.php');
+
+?>
+<script src="js/main.js"></script>
 </body>
 </html>
--- work/public/login.php
+++ work/public/login.php
@@ -1,6 +1,13 @@
 <?php
+session_start();
 require_once(__DIR__ . '/../app/config.php');
 require(__DIR__ . '/../app/functions.php');
+
+if (isset($_SESSION['form'])) {
+  $form = $_SESSION['form'];
+  var_dump($form['email']);
+}
+
 
 $title = 'ログイン - ';
 $this_css = 'form';
@@ -12,34 +19,47 @@ include('../app/_parts/_header.php');
 
 ?>
 
-  <div class="forms">
-    <div class="form_title">
-      <h1>ログイン</h1>
-    </div>
-    <div class="form">
-      <form action="my_page.php" method="post">
-        <div class="form_item">
-          <label for="name">ユーザーネーム</label>
-          <input type="text" id="name" maxlength="255" placeholder="（例）yume_miruko">
-        </div>
-        <div class="form_item">
-          <label for="email">メールアドレス</label>
-          <input type="email" id="email" maxlength="255" placeholder="（例）yumemiruko@gmail.com">
-        </div>
-        <div class="form_item">
-          <label for="password">パスワード</label>
-          <input type="password" id="password" maxlength="20" placeholder="半角英数8文字以上">
-        </div>
-        <button>ログイン</button>
-      </form>
-    </div>
+<div class="forms">
+  <div class="form_title">
+    <h1>ログイン</h1>
   </div>
+  <div class="form">
+    <form action="my_page.php" method="post">
+      <div class="form_item">
+        <label for="email">メールアドレス</label>
+        <input type="email" id="email" maxlength="255" placeholder="（例）yumemi@gmail.com" 
+        value="
+        <?php 
+          if (isset($_SESSION['form'])) {
+            echo h($form['email']);
+          }
+        ?>">
+        <div class="error">
+          <?php if(isset($error['email']) && $error['email'] === 'blank'): ?>
+          <p class="error">* メールアドレスを入力してください</p>
+          <?php endif; ?>
+          </div>
+      </div>
 
-  <?php
+      <div class="form_item">
+        <label for="password">パスワード</label>
+        <input type="password" id="password" maxlength="20" placeholder="（例）yume36ko">
+        <div class="error">
+          <?php if (isset($error['password']) && $error['password'] === 'alphanumeric'):?>
+          <p class="error">* パスワードを入力してください</p>
+          <?php endif; ?>
+        </div>
+      </div>
+      <button>ログイン</button>
+    </form>
+  </div>
+</div>
 
-  include('../app/_parts/_footer.php');
+<?php
 
-  ?>
-  <script src="js/main.js"></script>
+include('../app/_parts/_footer.php');
+
+?>
+<script src="js/main.js"></script>
 </body>
 </html>
