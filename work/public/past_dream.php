fe5361f9d296cb1500c2d9ad935a8183e8bcfde6
meri96bockn <meri96bockn@gmail.com>
2022-02-28 16:51:20 +0900

    コメントアウトしてしまっていたので再修正

--- work/public/past_dream.php
+++ work/public/past_dream.php
@@ -1,38 +1,43 @@
 <?php
 require_once(__DIR__ . '/../app/config.php');
-require('../app/functions.php');
+require(__DIR__ . '/../app/functions.php');
 
 $post_id = filter_input(INPUT_GET, 'post_id', FILTER_SANITIZE_NUMBER_INT);
-// if (isset($_SESSION['name']) && isset($_SESSION['id']) && isset($post_id)) {
-//   createToken();
-//   $id = $_SESSION['id'];
-//   $name = $_SESSION['name'];
-// } else {
-//   header('Location: login.php');
-//   exit;
-// }
-$id = '1';
-
+if (isset($_SESSION['name']) && isset($_SESSION['id']) && isset($post_id)) {
+  createToken();
+  $id = $_SESSION['id'];
+  $name = $_SESSION['name'];
+} else {
+  header('Location: login.php');
+  exit;
+}
 
 $stmt = $pdo->prepare(
-  "SELECT * FROM posts WHERE id = :post_id AND member_id = :members_id"
+  "SELECT *
+  FROM posts
+  WHERE id = :post_id AND member_id = :members_id"
 );
-$stmt->execute([
-  ':post_id' => $post_id,
-  ':members_id' => $id
-]);
+$stmt->execute(
+  [
+    ':post_id' => $post_id,
+    ':members_id' => $id
+  ]
+);
 $past_dreams = $stmt->fetchAll();
-
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['type']) && $_POST['type'] === 'action') {
   try {
     $stmt = $pdo->prepare(
-      "UPDATE posts SET tag = 'no_tag', emotion = '' WHERE id = :post_id AND member_id = :members_id"
+      "UPDATE posts
+      SET tag = 'no_tag', emotion = ''
+      WHERE id = :post_id AND member_id = :members_id"
     );
-    $stmt->execute([
-      'post_id' => $post_id,
-      'members_id' => $id
-    ]);
+    $stmt->execute(
+      [
+        'post_id' => $post_id,
+        'members_id' => $id
+      ]
+    );
     $id = h($post_id);
     $url = "past_dream.php?post_id=" . $id;
     header('Location:' . $url);
@@ -42,65 +47,69 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['type']) && $_POST['ty
     $error['try'] = "failure";
     $error_message = 'Error:'. $e->getMessage();
     error_log($error_message, 1, "error@dreamuseum.com");
-    die();
   }
 }
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['type']) && $_POST['type'] === 'yes') {
   $emotion = filter_input(INPUT_POST, 'emotion', FILTER_SANITIZE_STRING);
-  // try {
+  try {
     $stmt = $pdo->prepare(
-      "UPDATE posts SET tag = 'yes_tag', emotion = :emotion WHERE id = :post_id AND member_id = :members_id"
+      "UPDATE posts
+      SET tag = 'yes_tag', emotion = :emotion
+      WHERE id = :post_id AND member_id = :members_id"
     );
-    $stmt->execute([
-      'emotion' => $emotion,
-      'post_id' => $post_id,
-      'members_id' => $id
-    ]);
+    $stmt->execute(
+      [
+        'emotion' => $emotion,
+        'post_id' => $post_id,
+        'members_id' => $id
+      ]
+    );
     $id = h($post_id);
     $url = "past_dream.php?post_id=" . $id;
     header('Location:' . $url);
     exit;
-  // } catch (PDOException $e) {
-  //   $pdo->rollBack();
-  //   $error['try'] = "failure";
-  //   $error_message = 'Error:'. $e->getMessage();
-  //   error_log($error_message, 1, "error@dreamuseum.com");
-  //   die();
-
-}
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['type']) &&  $_POST['type'] === 'bye') {
-  try {
-    $stmt = $pdo->prepare(
-      "DELETE FROM posts WHERE id = :post_id AND member_id = :members_id"
-    );
-    $stmt->execute([
-      ':post_id' => $post_id,
-      ':members_id' => $id
-    ]);
-    header('Location: my_page.php');
   } catch (PDOException $e) {
     $pdo->rollBack();
     $error['try'] = "failure";
     $error_message = 'Error:'. $e->getMessage();
     error_log($error_message, 1, "error@dreamuseum.com");
-    die();
+  }
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['type']) &&  $_POST['type'] === 'bye') {
+  try {
+    $stmt = $pdo->prepare(
+      "DELETE FROM posts
+      WHERE id = :post_id AND member_id = :members_id"
+    );
+    $stmt->execute(
+      [
+        ':post_id' => $post_id,
+        ':members_id' => $id
+      ]
+    );
+    header('Location: my_page.php');
+    exit;
+  } catch (PDOException $e) {
+    $pdo->rollBack();
+    $error['try'] = "failure";
+    $error_message = 'Error:'. $e->getMessage();
+    error_log($error_message, 1, "error@dreamuseum.com");
   }
 }
 
 $title = 'むかしの夢 - ';
 $this_css = 'post';
 $my_page = 'select';
-include('../app/_parts/_header.php');
-
+include(__DIR__ . '/../app/_parts/_header.php');
 ?>
 
 <?php if (isset($error['try']) && $error['try'] === 'failure'): ?>
   <div class="container">
-      <div class="error">
-        <p>* お手数ですが、もう一度やり直してください</p>
-      </div>
+    <div class="error">
+      <p>* お手数ですが、もう一度やり直してください</p>
+    </div>
   </div>
 
 <?php else: ?>
@@ -111,33 +120,26 @@ include('../app/_parts/_header.php');
       <?php foreach ($past_dreams as $past_dream):?>
         <div class="tag">
           <?php if (isset($past_dream['tag']) && ($past_dream['tag'] === 'yes_tag')): ?>
-              <i class="bi bi-award gold"></i>
+            <i class="bi bi-award gold"></i>
           <?php endif; ?>
           <?php if (isset($past_dream['tag']) && ($past_dream['tag'] === 'no_tag')): ?>
-              <i class="bi bi-award normal"></i>
+            <i class="bi bi-award normal"></i>
           <?php endif; ?>
           <div class="emotion">
             <?php if ($past_dream['tag'] === 'yes_tag'): ?>
-              <i class="bi bi-tag-fill"></i><?= h($past_dream['emotion']); ?>
+            <i class="bi bi-tag-fill"></i><?= h($past_dream['emotion']); ?>
             <?php endif; ?>
           </div>
         </div>
         <div>
-          <h1>
-          <?= h($past_dream['title']); ?>
-          </h1>
+          <h1><?= h($past_dream['title']); ?></h1>
         </div>
         <div class="content">
           <?= nl2br(h($past_dream['content'])); ?>
         </div>
         <div class="info">
-          <p>
-          <?= h($past_dream['created']); ?>
-          </p>
-          <p>
-            <i class="bi bi-person-circle"></i>
-            <?= h($name); ?>
-          </p>
+          <p><?= h($past_dream['created']); ?></p>
+          <p><i class="bi bi-person-circle"></i><?= h($name); ?></p>
         </div>
         <div class="button">
           <?php if ($past_dream['tag'] === 'yes_tag'): ?>
